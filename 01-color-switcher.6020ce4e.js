ba3dbf22df9cd53b7d241eef47fee2b8d6dfae27
AnnaSil <108275504+AnnaSil@users.noreply.github.com>
2023-02-16 19:45:23 +0000

    Deploying to gh-pages from @ AnnaSil/goit-js-hw-09@6b64d69c3003959a8c89a567e2efc71ba8e109e0 🚀

---
+++ 01-color-switcher.6020ce4e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){if(isActive)return;isActive=!0,timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),isActive=!1})),timerId=null,isActive=!1;
+//# sourceMappingURL=01-color-switcher.6020ce4e.js.map
---
+++ 01-color-switcher.696a2751.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){if(isActive)return;isActive=!0,timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),isActive=!1})),timerId=null,isActive=!1}();
+//# sourceMappingURL=01-color-switcher.696a2751.js.map
